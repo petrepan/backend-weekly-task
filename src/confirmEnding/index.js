2874b42f8a9cb02b08bb66f80d304e6c2f467300
Moses <mosesojiko999@gmail.com>
2021-06-09 06:42:39 -0700

    Solved all the weekly task

--- src/confirmEnding/index.js
+++ src/confirmEnding/index.js
@@ -1,6 +1,13 @@
 function confirmEnding(str, target) {
-  
+  let len = target.length;
+  let ending = str.slice(str.length-len)
+  if(ending === target){
+      return true
+  }else{
+      return false;
+  }
 }
 
 confirmEnding("Bastian", "n") // => true
 confirmEnding("Congratulation", "on") // => true
+confirmEnding("Connor", "n") // => false
--- src/truncate/index.js
+++ src/truncate/index.js
@@ -1,5 +1,6 @@
 function truncateString(str, num) {
- 
+ let truncated = str.slice(0,num) + "...";
+ return str.length > num? truncated: str;
 }
 
 truncateString("A-tisket a-tasket A green and yellow basket", 8) // => A-tisket...
