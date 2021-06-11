106bd3809ef6e891b99bb7c94e41b8e52e5f15e9
solomon-prosperity <solomonprosperityera@gmail.com>
2021-06-11 20:21:30 +0800

    index.js

--- src/confirmEnding/index.js
+++ src/confirmEnding/index.js
@@ -1,5 +1,10 @@
 function confirmEnding(str, target) {
-  
+    let endChecker = str.charAt(str.length - 1)
+    if (endChecker === target ) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 confirmEnding("Bastian", "n") // => true
