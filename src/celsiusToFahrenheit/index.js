152bf1390521ed2397008fcd2840da76e42836bc
Chukwunalu Egwali <chukeg2012@gmail.com)>
2021-06-14 22:54:28 +0100

    added js files to the HTML file

--- src/celsiusToFahrenheit/index.js
+++ src/celsiusToFahrenheit/index.js
@@ -1,12 +1,10 @@
-//This function takes in one number type as its parameter
-//The function converts convert from Celsius to Fahrenheit.
-//The formular to convert to fahrenheit is "temperature in Celsius times 9 / 5, plus 32".
+/*the algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+*/
 
-function convertToFahrenheit(celsius) {
-    let fahrenheit;
-    return fahrenheit;
+function convertToF(celcius){ 
+  let fahrenheit = celcius * 9/5 + 32;
+  alert(fahrenheit)
 }
-
-
-convertToF(-30) // -> -22
-convertToF(-10) // -> 14
+convertToF(-30) // -22
+convertToF(-10) // 14
+convertToF(0)  // 32
