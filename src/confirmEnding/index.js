5254adfa1f234e6fb75252b984335da2c3d3c4f4
profalphon <proforeva@yahoo.com>
2021-06-14 23:17:39 +0100

    Updated formulas

--- src/confirmEnding/index.js
+++ src/confirmEnding/index.js
@@ -1,6 +1,20 @@
 function confirmEnding(str, target) {
-  
+  return str.slice(str.length - target.length) === target 
 }
 
-confirmEnding("Bastian", "n") // => true
-confirmEnding("Congratulation", "on") // => true
+alert(confirmEnding("Bastian", "n"));
+alert(confirmEnding("Congratulation", "on"));
+alert(confirmEnding("Connor", "n"));
+
+// OR MAKING USE OF SLICE 
+
+
+
+function confirmEnding(str, target) {
+    let ending = str.slice(str.length - target.length)
+    return ending === target 
+  }
+
+alert(confirmEnding("Bastian", "n"));
+alert(confirmEnding("Congratulation", "on"));
+alert(confirmEnding("Connor", "n"));
