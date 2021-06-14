e43537c561dd7a736a4fada27a5e7ae66b9bc2e4
MABEL1045 <mabelvictoria32@gmail.com>
2021-06-14 21:53:00 +0100

    First commit

--- src/celsiusToFahrenheit/index.js
+++ src/celsiusToFahrenheit/index.js
@@ -1,12 +1,11 @@
-//This function takes in one number type as its parameter
-//The function converts convert from Celsius to Fahrenheit.
-//The formular to convert to fahrenheit is "temperature in Celsius times 9 / 5, plus 32".
 
-function convertToFahrenheit(celsius) {
-    let fahrenheit;
-    return fahrenheit;
-}
+function convertToF(celsius){
+    alert ((celsius * 9/5) + 32)
+         
+    }
+    convertToF(-30) // -> -22
+    convertToF(-10) // -> 14
+    convertToF(0)// -> 32
+    
 
 
-convertToF(-30) // -> -22
-convertToF(-10) // -> 14
--- src/confirmEnding/index.js
+++ src/confirmEnding/index.js
@@ -1,6 +1,13 @@
 function confirmEnding(str, target) {
-  
+    if (str.substr(-target.length) == target) {
+     alert("true")
+    
+ }  
+ else {
+     alert("false")
+}
 }
 
-confirmEnding("Bastian", "n") // => true
-confirmEnding("Congratulation", "on") // => true
+confirmEnding("Bastian", "n")// => true
+confirmEnding("Congratulation", "on") //=> true
+confirmEnding("Connor", "n")// => false
