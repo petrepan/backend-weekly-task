c771849466bdd6ad3bfc9344552deb0fae7d0f21
unknown <erhijotajonah25@gmail.com>
2021-06-15 10:54:32 -0700

    commit

--- src/confirmEnding/index.js
+++ src/confirmEnding/index.js
@@ -1,6 +1,13 @@
 function confirmEnding(str, target) {
-  
+  if (str.substr(-target.length)=== target){
+return true;
+  } return false;
 }
 
 confirmEnding("Bastian", "n") // => true
 confirmEnding("Congratulation", "on") // => true
+confirmEnding("Connor", "n") //=> false
+
+alert (confirmEnding("Bastian", "n"))
+alert (confirmEnding("Congratulation", "on"))
+alert (confirmEnding("Connor", "n"))
--- src/truncate/index.js
+++ src/truncate/index.js
@@ -1,6 +1,14 @@
 function truncateString(str, num) {
+ if (num <= str.length) {
+    return str.slice(0,num) + "..."
+ }
+ return str;
+ }
  
-}
-
+truncateString("A-", 1) // => A....
 truncateString("A-tisket a-tasket A green and yellow basket", 8) // => A-tisket...
 truncateString("Absolutely Longer", 2) // => Ab....
+
+alert(truncateString("A-", 1))
+alert(truncateString("A-tisket a-tasket A green and yellow basket", 8))
+alert(truncateString("Absolutely Longer", 2))
