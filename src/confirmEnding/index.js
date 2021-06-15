8ab9deb5f9a4f4df022dc58bc01b170f766e3e02
unknown <omatsolaseund@gmail.com>
2021-06-15 09:55:18 +0100

    done assignment

--- src/confirmEnding/index.js
+++ src/confirmEnding/index.js
@@ -1,6 +1,10 @@
 function confirmEnding(str, target) {
-  
+  if (str.endsWith(target) ) {
+      return true;
+  } else {
+      return false;
+  }
 }
 
 confirmEnding("Bastian", "n") // => true
-confirmEnding("Congratulation", "on") // => true
+alert(confirmEnding("Congratulation", "on")) // => true
