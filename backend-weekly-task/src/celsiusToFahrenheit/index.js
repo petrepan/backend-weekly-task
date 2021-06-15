909b28da99000219005b2de7f653b234eb8cccee
Lovinaj <85228654+Lovinaj@users.noreply.github.com>
2021-06-15 13:56:15 +0100

    WEEK TWO ASSIGNMENT

---
+++ backend-weekly-task/src/celsiusToFahrenheit/index.js
@@ -0,0 +1,10 @@
+//This function takes in one number type as its parameter
+//The function converts convert from Celsius to Fahrenheit.
+//The formular to convert to fahrenheit is "temperature in Celsius times 9 / 5, plus 32".
+
+function convertToFahrenheit(celsius) {
+    let fahrenheit = celsius * 9/5 + 32;
+    return fahrenheit;
+}
+alert(convertToFahrenheit(-30)); // -> -22
+alert(convertToFahrenheit(-10)) // -> 14
