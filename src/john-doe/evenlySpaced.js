24609d0ab7bf3485a40c7b5808a69613772f9e30
petrepan <peterawotola@gmail.com>
2021-06-19 10:42:14 +0100

    task setup

---
+++ src/john-doe/evenlySpaced.js
@@ -0,0 +1,10 @@
+
+function evenlySpaced(first, second, last) {
+    //Provide your solution here
+    
+}
+
+evenlySpaced(2,4,6) // -> should return true
+evenlySpaced(3,6,12) // -> should return false
+
+module.exports = evenlySpaced;
