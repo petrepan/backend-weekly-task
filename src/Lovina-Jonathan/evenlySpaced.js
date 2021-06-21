b1f146f63b88f904022dcd669544618c495e971b
Lovinaj <85228654+Lovinaj@users.noreply.github.com>
2021-06-21 14:03:21 +0100

    Add files via upload
    Assignment For Week 3

---
+++ src/Lovina-Jonathan/evenlySpaced.js
@@ -0,0 +1,15 @@
+
+function evenlySpaced(first, second, last) {
+    //Provide your solution here
+    if ((second - first)===(last - second)) {
+        return true
+    } else {
+        return false
+    } 
+        
+}
+
+evenlySpaced(2,4,6) // -> should return true
+evenlySpaced(3,6,12) // -> should return false
+
+module.exports = evenlySpaced;
