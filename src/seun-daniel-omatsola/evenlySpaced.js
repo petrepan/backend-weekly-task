b444a59f6e12be731ad58506252c7020898193c2
Seun Daniel Omatsola <omatsolaseund@gmail.com>
2021-06-25 14:09:18 +0000

    Seun Daniel Omatsola assignment week3 (#5)
    * done assignment
    
    * done the assignment on evenlySpace
    
    * removed john-doe folder
    
    * corrected the folder issue

---
+++ src/seun-daniel-omatsola/evenlySpaced.js
@@ -0,0 +1,16 @@
+
+function evenlySpaced(first, second, last) {
+    //Provide your solution here
+    if (second - first === last - second) {
+        return true;
+    } else if (first - second === second - last) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+evenlySpaced(2,4,6); // -> should return true
+evenlySpaced(3,6,9); // -> should return false
+
+module.exports = evenlySpaced;
